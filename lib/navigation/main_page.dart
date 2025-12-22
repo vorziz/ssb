303dac3aa3d961be287360e317d6db7c2e5f60b0
Vorziz <admin@digikreatif.id>
2025-12-22 20:33:35 +0700

    fix bottom navigation bar

---
+++ lib/navigation/main_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../features/services/ui/service_list_page.dart';
+import '../features/wallet/ui/wallet_page.dart';
+import '../features/rewards/ui/rewards_page.dart';
+import '../features/home/ui/home_page.dart';
+
+import 'bottom_nav_controller.dart';
+
+class MainPage extends StatelessWidget {
+  MainPage({super.key});
+
+  final BottomNavController navController = Get.find();
+
+  final List<String> titles = [
+    'Home',
+    'Services',
+    'Wallet',
+    'Rewards',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Scaffold(
+        appBar: AppBar(
+          title: Text(titles[navController.currentIndex.value]),
+        ),
+        body: IndexedStack(
+          index: navController.currentIndex.value,
+          children: [
+            HomePage(),
+            ServiceListPage(),
+            WalletPage(),
+            RewardsPage(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: navController.currentIndex.value,
+          onTap: navController.changeTab,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Services',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet),
+              label: 'Wallet',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: 'Rewards',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
